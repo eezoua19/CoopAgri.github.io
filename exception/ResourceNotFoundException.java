2209665ed0fa8eea200f44151090e692d387cd0b
Emmanuel EZOUA <ezouaemmanuel07@gmail.com>
2026-01-12 16:55:43 +0000

    Create ResourceNotFoundException.java

---
+++ exception/ResourceNotFoundException.java
@@ -0,0 +1,17 @@
+package com.coopagri.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+    
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+    
+    public ResourceNotFoundException(String resourceName, String fieldName, Object fieldValue) {
+        super(String.format("%s non trouvé(e) avec %s : '%s'", 
+            resourceName, fieldName, fieldValue));
+    }
+}
